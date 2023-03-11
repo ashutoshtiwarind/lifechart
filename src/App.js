7a6e8384bd044e3fd2a1a0d8906ed5467fa4ffed
ashutosh tiwari <ashutoshtiwarindia@gmail.com>
2023-03-11 11:01:49 +0530

    added header
    added some label in header component and css style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { Header } from "./components/header/Header";
+
 function App() {
   return (
     <div>
+      <Header />
       life chart
     </div>
   );
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+export const Header = () => {
+  return <div>
+    <div className="header-main">
+      <div>home</div>
+      <div>graph</div>
+      <div>account</div>
+    </div>
+  </div>
+}
